97a34886a96e778fdabf3db14db2de194f968e8b
mercuridi <kaibarberharris@protonmail.com>
2025-07-21 15:10:15 +0100

    set up dummy files for testing + structure

---
+++ dev/check_actions.py
@@ -0,0 +1,8 @@
+# File to check if automatic linting is working
+
+def main():
+    print("File running check")
+    return True
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_check_actions.py
@@ -0,0 +1,6 @@
+import pytest
+
+from dev.check_actions import main
+
+def test_main():
+    assert main()
