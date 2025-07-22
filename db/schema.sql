46b9ce52968597c4db3f62692e28a08e8b0c7b78
mercuridi <kaibarberharris@protonmail.com>
2025-07-22 10:07:08 +0100

    working on schema

---
+++ db/schema.sql
@@ -0,0 +1,35 @@
+drop table if exists botanist cascade;
+drop table if exists plant cascade;
+drop table if exists reading cascade;
+drop table if exists city cascade;
+drop table if exists country cascade;
+drop table if exists photo cascade;
+drop table if exists origin cascade;
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
+
+create table botanist (
+
+);
