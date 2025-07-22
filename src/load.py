407313bbaedaf269c69fd9633f01d459913c5fa6
mercuridi <kaibarberharris@protonmail.com>
2025-07-22 14:55:20 +0100

    OOP refactor

--- src/load.py
+++ src/load.py
@@ -1,53 +1,54 @@
 """Script to load cleaned data into the SQL Server RDS"""
+
+"""
+(DONE) Takes in a dataframe of all the data we need
+(WORK) Construct insertion queries from the dataframe in a dict
+(WORK)    Country insertions
+(WORK)    City insertions
+(WORK)    Origin insertions
+(WORK)    Plant insertions
+(WORK)    Photo insertions
+(WORK)    Botanist insertions
+(WORK)    Reading insertions
+(DONE) Makes a connection to the RDS
+(WORK) Runs insertions
+(DONE) Closes connections gracefully
+"""
+
 import os 
 
 import dotenv
 import pandas as pd
 import pymssql
 
-def main(df: pd.DataFrame = pd.DataFrame()):
-    """
-    (WORK) Takes in a dataframe of all the data we need
-    (WORK) Construct insertion queries from the dataframe in a dict
-    (WORK)    Country insertions
-    (WORK)    City insertions
-    (WORK)    Origin insertions
-    (WORK)    Plant insertions
-    (WORK)    Photo insertions
-    (WORK)    Botanist insertions
-    (WORK)    Reading insertions
-    (DONE) Makes a connection to the RDS
-    (WORK) Runs insertions
-    (DONE) Closes connections gracefully
-    """
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError(f"Input to load stage must be a dataframe; received {type(df)}")
-    if len(df) == 0:
-        raise ValueError("Dataframe must contain data.")
+class DataLoader:
+    def __init__(self, df):
+        if not isinstance(df, pd.DataFrame):
+            raise ValueError(f"Input to load stage must be a dataframe; received {type(df)}")
+        if len(df) == 0:
+            raise ValueError("Dataframe must contain data.")
 
-    queries = {}
+        self.data = df
+        self.conn = pymssql.connect(
+            os.environ["DB_HOST"],
+            os.environ["DB_USER"],
+            os.environ["DB_PASSWORD"],
+            os.environ["DB_NAME"]
+        )
 
-    conn = get_rds_conn()
-    cur = conn.cursor()
-    cur.execute("select * from country;")
-    vals = cur.fetchall()
-    print(vals)
-    cur.close()
-    conn.close()
-
-
-def get_rds_conn() -> pymssql.Connection:
-    """
-    Function to easily get an RDS connection
-    Requires the .env to be loaded
-    """
-    return pymssql.connect(
-        os.environ["DB_HOST"],
-        os.environ["DB_USER"],
-        os.environ["DB_PASSWORD"],
-        os.environ["DB_NAME"]
-    )
+        self.queries = {}
+    
+    def close_conn(self):
+        self.conn.close()
+    
+    def collect_queries(self):
+        cur = self.conn.cursor()
+        cur.execute("select * from country;")
+        vals = cur.fetchall()
+        print(vals)
 
 if __name__ == "__main__":
     dotenv.load_dotenv()
-    main()
+    df = pd.DataFrame()
+    loader = DataLoader(df)
+    loader.collect_queries()
