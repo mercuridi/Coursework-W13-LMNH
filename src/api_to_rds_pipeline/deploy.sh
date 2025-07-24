a1d37fe15acab59a931cbae09bec3bceb21f464c
james-plw <james.wright2@bath.edu>
2025-07-24 15:49:00 +0100

    Fixed build command

--- src/api_to_rds_pipeline/deploy.sh
+++ src/api_to_rds_pipeline/deploy.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 129033205317.dkr.ecr.eu-west-2.amazonaws.com
 
-docker build -f src/api_to_rds_pipeline/dockerfile -t c18-git-botanists-etl-repo .
+docker buildx build --platform linux/amd64 --provenance=false -t c18-git-botanists-etl-repo -f src/api_to_rds_pipeline/dockerfile .
 
 docker tag c18-git-botanists-etl-repo:latest 129033205317.dkr.ecr.eu-west-2.amazonaws.com/c18-git-botanists-etl-repo:latest
 
