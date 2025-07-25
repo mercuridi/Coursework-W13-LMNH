c89224aaa87189934699d55fb3433bf6aecd8dd1
james-plw <james.wright2@bath.edu>
2025-07-25 12:37:18 +0100

    More permissions

--- terraform/s3_tf/main.tf
+++ terraform/s3_tf/main.tf
@@ -40,12 +40,13 @@ resource "aws_iam_role" "glue_service_role" {
 
 # Glue permissions
 resource "aws_iam_role_policy" "glue_inline_permissions" {
-  name = "glue-catalog-s3-access"
+  name = "c18-botanists-glue-s3-access"
   role = aws_iam_role.glue_service_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
+      # Glue Catalog Permissions
       {
         Effect = "Allow",
         Action = [
@@ -53,11 +54,18 @@ resource "aws_iam_role_policy" "glue_inline_permissions" {
           "glue:UpdateTable",
           "glue:GetTable",
           "glue:GetTables",
+          "glue:GetPartition",
+          "glue:GetPartitions",
+          "glue:BatchGetPartition",
+          "glue:BatchCreatePartition",
           "glue:GetDatabase",
-          "glue:GetDatabases"
+          "glue:GetDatabases",
+          "glue:UpdatePartition"
         ],
         Resource = "*"
       },
+
+      # S3 Bucket Permissions
       {
         Effect = "Allow",
         Action = [
@@ -71,6 +79,8 @@ resource "aws_iam_role_policy" "glue_inline_permissions" {
           "arn:aws:s3:::c18-botanists-s3-bucket/*"
         ]
       },
+
+      # CloudWatch Logs Permissions
       {
         Effect = "Allow",
         Action = [
@@ -82,17 +92,11 @@ resource "aws_iam_role_policy" "glue_inline_permissions" {
           "arn:aws:logs:eu-west-2:129033205317:log-group:/aws-glue/*",
           "arn:aws:logs:eu-west-2:129033205317:log-group:/aws-glue/*:log-stream:*"
         ]
-      },
-          {
-      Effect = "Allow",
-      Action = [
-        "glue:GetDatabase"
-      ],
-      Resource = "arn:aws:glue:eu-west-2:129033205317:catalog"
-    }
+      }
     ]
   })
 }
+
 
 
 
