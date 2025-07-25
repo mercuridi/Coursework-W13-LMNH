55785e1a245f9cd1093f9449cb307b21014975ff
mercuridi <kaibarberharris@protonmail.com>
2025-07-25 12:25:13 +0100

    refactor function to test properly

--- src/api_to_rds_pipeline/load.py
+++ src/api_to_rds_pipeline/load.py
@@ -195,15 +195,6 @@ class DataLoader:
 
         logging.debug("ID for %s: %s", table_name, val)
         return val
-    
-
-    def check_table_name_valid(self, table_name: str):
-        """Check if a table name is in the list of known tables before we try to query it"""
-        logging.debug("Checking table name %s is valid", table_name)
-        if table_name not in RDS_TABLES_WITH_FK:
-            raise ValueError(f"Given table name {table_name} is not a known destination")
-        logging.debug("Table name OK")
-        return True
 
 
     def close_conn(self):
@@ -212,6 +203,14 @@ class DataLoader:
         self.conn.close()
         logging.info("RDS connection closed")
 
+
+def check_table_name_valid(table_name: str):
+    """Check if a table name is in the list of known tables before we try to query it"""
+    logging.debug("Checking table name %s is valid", table_name)
+    if table_name not in RDS_TABLES_WITH_FK:
+        raise ValueError(f"Given table name {table_name} is not a known destination")
+    logging.debug("Table name OK")
+    return True
 
 # Example usage
 
--- test/test_atr_load.py
+++ test/test_atr_load.py
@@ -5,35 +5,29 @@ import pytest
 import dotenv
 
 from src.api_to_rds_pipeline.transform import PlantDataTransformer
-from src.api_to_rds_pipeline.load import RDS_TABLES_WITH_FK, DataLoader
+from src.api_to_rds_pipeline.load import RDS_TABLES_WITH_FK, check_table_name_valid
 from test_atr_transform import EXAMPLE
 
-@pytest.fixture
-def dataloader():
-    dotenv.load_dotenv()
-    transformer = PlantDataTransformer(EXAMPLE)
-    return DataLoader(transformer.transform())
+def test_check_table_name_valid_bad_input():
+    with pytest.raises(ValueError):
+        check_table_name_valid("bad")
+    with pytest.raises(ValueError):
+        check_table_name_valid(1)
+    with pytest.raises(ValueError):
+        check_table_name_valid("")
+    with pytest.raises(ValueError):
+        check_table_name_valid(None)
+    with pytest.raises(ValueError):
+        check_table_name_valid(True)
+    with pytest.raises(ValueError):
+        check_table_name_valid(False)
 
-def test_check_table_name_valid_bad_input(dataloader):
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid("bad")
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid(1)
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid("")
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid(None)
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid(True)
-    with pytest.raises(ValueError):
-        dataloader.check_table_name_valid(False)
-
-def test_check_table_name_valid_good_input(dataloader):
-    assert dataloader.check_table_name_valid("country")
-    assert dataloader.check_table_name_valid("city")
-    assert dataloader.check_table_name_valid("origin")
-    assert dataloader.check_table_name_valid("botanist")
-    assert dataloader.check_table_name_valid("plant")
-    assert dataloader.check_table_name_valid("reading")
-    assert dataloader.check_table_name_valid("photo")
+def test_check_table_name_valid_good_input():
+    assert check_table_name_valid("country")
+    assert check_table_name_valid("city")
+    assert check_table_name_valid("origin")
+    assert check_table_name_valid("botanist")
+    assert check_table_name_valid("plant")
+    assert check_table_name_valid("reading")
+    assert check_table_name_valid("photo")
 
